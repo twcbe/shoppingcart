aa6be2599c435532d2228def232dd4c740123702
Abdul Khader Jeelani <abdulkj@thoughtworks.com>
2019-06-18 01:30:14 +0530

    [Abdul] [Refactored: Tail call optimization, Invert additional price calculation]

--- src/thoughtworks/main.kt
+++ src/thoughtworks/main.kt
@@ -1,57 +1,58 @@
+data class Product(val id: String, val name: String, val price: Double, val category: String)
+
 fun main(args: Array<String>) {
 
-
-    val productsCatlog = listOf(
+    val productsCatalog = listOf(
         Product("P5", "Agile", 100.00, "Book"),
         Product("P6", "FP", 350.00, "Book"),
         Product("P7", "Kotlin", 250.00, "Book")
     )
 
-    val userItems = getProductFromUser(productsCatlog, arrayOf()).map { "${it.itemPrice}, ${it.productId}" }
+    val userItems = getCartItems(productsCatalog, arrayOf()).map { "${it.unitPrice}, ${it.productId}" }
     userItems.map { println(it) }
 }
 
-data class CartItem(val productId: String, val quantity: Int, val itemPrice: Double);
+data class CartItem(val productId: String, val quantity: Int, val unitPrice: Double)
 
-
-fun getProductFromUser(catalog: Collection<Product>, cartItems: Array<CartItem>): Array<CartItem> {
-    val productId = promptUser("Enter the prdouct to wish for")
-    val product = catalog.first { it.id == productId }
-    val cartItem = if (product.category == "Book") getBookFromUser(product) else CartItem(product.id, 1, product.price)
-    val continues = promptUser("Enter c to add more products")
-    val newCart = arrayOf(*(cartItems), cartItem)
-    return if (continues == "c") getProductFromUser(catalog, newCart) else newCart
+tailrec fun promptUser(prompt: String = "", validValues: List<String> = emptyList()): String {
+    println(prompt)
+    val value = readLine() ?: ""
+    return if (validValues.isEmpty() || validValues.contains(value)) value else promptUser(prompt, validValues)
 }
 
-fun getBookFromUser(book: Product): CartItem {
-    val bookType = promptUser("Enter E for Ebook, P for Paperback")
-    val bookSubType = when (bookType) {
-        "E" -> promptUser("Enter K for kindle, P for Pdf")
-        "P" -> promptUser("Enter H for Hardcover, S for Softcover")
-        else -> ""
+fun getProduct(catalog: Collection<Product>): Product {
+    val productId = promptUser("Enter the product id wish for", catalog.map {
+        it.id
+    })
+    return catalog.first { it.id == productId }
+}
+
+tailrec fun getCartItems(catalog: Collection<Product>, cartItems: Array<CartItem>): Array<CartItem> {
+    val product = getProduct(catalog);
+    val newCart = arrayOf(
+        *(cartItems),
+        CartItem(product.id, 1, getUnitPrice(product, ::getBookTypes))
+    )
+    val canContinue = promptUser("Enter C to add more products, X to Complete", listOf("C", "X")) == "C"
+    return if (canContinue) getCartItems(catalog, newCart) else newCart
+}
+
+tailrec fun getBookTypes(): Pair<String, String> =
+    when (promptUser("Enter E for EBook, P for Paperback", listOf("E", "P"))) {
+        "E" -> Pair("E", promptUser("Enter K for kindle, P for Pdf", listOf("K", "P")))
+        "P" -> Pair("P", promptUser("Enter H for Hardcover, S for Softcover", listOf("H", "S")))
+        else -> getBookTypes()
     }
-    return CartItem(book.id, 1, book.price + getAdditionalPrice(bookType, bookSubType))
-}
 
-fun getAdditionalPrice(bookType: String, bookSubType: String): Double =
-    when (Pair(bookType, bookSubType)) {
+fun getUnitPrice(product: Product, bookTypesGetter: () -> Pair<String, String>) =
+    product.price + (if (product.category == "Book") getAdditionalPriceOfBook(bookTypesGetter()) else 0.0)
+
+fun getAdditionalPriceOfBook(bookTypes: Pair<String, String>): Double =
+    when (bookTypes) {
         Pair("E", "K") -> 100.0
         Pair("E", "P") -> 150.00
         Pair("P", "H") -> 150.00
         Pair("P", "S") -> 100.00
-        else -> 0.0;
+        else -> 0.0
     }
-
-
-fun promptUser(prompt: String = ""): String {
-    println(prompt)
-    return readLine() ?: ""
-}
-
-data class Product(val id: String, val name: String, val price: Double, val category: String);
-
-
-
-
-
 
